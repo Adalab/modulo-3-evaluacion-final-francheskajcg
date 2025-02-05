f8c4f488dbd5dc643bb2f855c07d7c47b3e13f23
Francheska Contreras Garcia <frankeka.jose@gmail.com>
2025-02-05 23:52:11 +0100

    rendering the list with the Json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 import '../styles/App.scss';
 
+import moviesJson from '../data/movies.json';
+
+
+
 function App() {
+
+  //VARIABLES DE ESTADO
+
+  const [movies, setMovies] = useState(moviesJson);
+
+
   return (
     <div>
       <header className='header'>
@@ -8,33 +20,25 @@ function App() {
         <p className='header_text'>Busca el WOW de Owen Wilson que mas te guste</p>
       </header>
 
-      <div className="search_container">
-        <label htmlFor="">Película</label>
-        <input type="text" />
+      <form className="search_container">
+        <label htmlFor="movie">Película:</label>
+        <input type="search" placeholder="Nombre de la película" />
 
-        <label htmlFor="">Año</label>
-        <input type="text" />
-      </div>
+        <label htmlFor="">Año:</label>
+        <input type="search" placeholder="Año de la película" />
+      </form>
 
-      <section className="list-container">
-        <div className="card">
-          <img src="https://m.media-amazon.com/images/I/91F43kQqbNL._AC_UF894,1000_QL80_.jpg" alt="" />
-          <h3>Nombre de la peli y año</h3>
-          <p>Wow.</p>
-        </div>
+      <ul className="list-container">
 
-        <div className="card">
-          <img src="https://m.media-amazon.com/images/I/91F43kQqbNL._AC_UF894,1000_QL80_.jpg" alt="" />
-          <h3>Nombre de la peli y año</h3>
-          <p>Wow.</p>
-        </div>
+        {movies.map((oneMovie) => <li key={oneMovie.timestamp} 
+        className="card">
+          <img src= {oneMovie.poster} alt="" />
+          <h3>{oneMovie.movie} {oneMovie.year}</h3>
+          <p>{oneMovie.full_line}</p>
+        </li>
+        )}
 
-        <div className="card">
-          <img src="https://m.media-amazon.com/images/I/91F43kQqbNL._AC_UF894,1000_QL80_.jpg" alt="" />
-          <h3>Nombre de la peli y año</h3>
-          <p>Yeah. Wow.</p>
-        </div>
-      </section>
+      </ul>
     </div>
   );
 }
